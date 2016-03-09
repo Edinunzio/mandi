37bc804d115407af73683ddc6314354bdfb8c2e5
Edinunzio <e.dinunzio@gmail.com>
2016-03-09 02:02:44 -0500

    stubbing view tdd style

--- tileslide/urls.py
+++ tileslide/urls.py
@@ -5,8 +5,9 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 """
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from puzzles.views import home_page
 
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
-    url(r'^$', 'puzzles.views.home_page', name='home'),
+    url(r'^$', home_page, name='home'),
 ]
