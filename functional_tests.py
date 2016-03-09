6781a9eab36833666c0d7b7fc64063b81b5c157d
Edinunzio <e.dinunzio@gmail.com>
2016-03-09 01:31:03 -0500

    updating end to end test

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,30 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewPlayerTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_show_puzzle_and_win(self):
+		# Barry's ready to take a chance at sliding tiles.
+		# He visits the home page.
+		self.browser.get('http://localhost:8000')
+
+		# He sees the page title mentioning the name
+		self.assertIn('Sliding Tiles', self.browser.title)
+		self.fail('Finish the test!')
+
+		# He sees a puzzle loaded on the page
+
+		# He clicks a tile touching the blank space.
+
+		# He sees the tiles switch place upon click.
+
+		# He solves the puzzle and sees the congratulations message
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
