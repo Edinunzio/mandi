6b04ab76fcb1b556ffd4fcc7f095360aee79f888
Edinunzio <e.dinunzio@gmail.com>
2016-03-10 19:54:12 -0500

    tightening up duplicated hardcoded stuff

--- puzzles/models.py
+++ puzzles/models.py
@@ -7,9 +7,10 @@ class Puzzle(models.Model):
 	def generate_tiles(self, n):
 		try:
 			n = int(n)
-			tiles = list(range(0,(n*n)))
-			sorted_tiles = tiles[1:]
-			sorted_tiles.append(0)
-			return sorted_tiles, random.sample(tiles, (n*n))
+			if n >1:
+				tiles = list(range(0,(n*n)))
+				sorted_tiles = tiles[1:]
+				sorted_tiles.append(0)
+				return sorted_tiles, random.sample(tiles, (n*n))
 		except ValueError:
 			raise ValidationError(('%(n)s is not a number'), params={'n': n})
