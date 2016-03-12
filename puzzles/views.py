d4952466a116d1ca1392619e4a5adabb09ebd700
Edinunzio <e.dinunzio@gmail.com>
2016-03-12 16:21:14 -0500

    adding legal moves map to home page

--- puzzles/views.py
+++ puzzles/views.py
@@ -4,12 +4,13 @@ from django.core.exceptions import ValidationError
 from puzzles.models import Puzzle
 
 
-def home_page(request, n=3):
+def home_page(request, grid_size=3):
 	puzzle = Puzzle()
 	if request.method == 'POST':
 		try:
 			grid_size = int(request.POST['grid_size'])
 			sorted_tiles = puzzle.generate_tiles(grid_size)
+			legal_moves_map = puzzle.legal_moves_map(grid_size, sorted_tiles)
 			tiles = puzzle.shuffle_tiles(grid_size, sorted_tiles)
 			while puzzle.check_solvable(tiles) == False:
 				tiles = puzzle.shuffle_tiles(grid_size, sorted_tiles)
@@ -19,10 +20,10 @@ def home_page(request, n=3):
 		except ValidationError:
 			pass
 
-	sorted_tiles = puzzle.generate_tiles(n)
-	tiles = puzzle.shuffle_tiles(n, sorted_tiles)
+	sorted_tiles = puzzle.generate_tiles(grid_size)
+	tiles = puzzle.shuffle_tiles(grid_size, sorted_tiles)
 	while puzzle.check_solvable(tiles) == False:
-		tiles = puzzle.shuffle_tiles(n, sorted_tiles)
-	tile_size = 100/n
-	content = {'grid_size': n, 'sorted_tiles': sorted_tiles, 'tiles': tiles, 'tile_size': tile_size}
+		tiles = puzzle.shuffle_tiles(grid_size, sorted_tiles)
+	tile_size = 100/grid_size
+	content = {'grid_size': grid_size, 'sorted_tiles': sorted_tiles, 'tiles': tiles, 'tile_size': tile_size}
 	return render(request, 'home.html', content)
