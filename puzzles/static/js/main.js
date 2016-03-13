3f3f0469700d97d790bf57a7175182bc347f0140
Edinunzio <e.dinunzio@gmail.com>
2016-03-13 06:08:41 -0400

    finally fixed stupid bug

--- puzzles/static/js/main.js
+++ puzzles/static/js/main.js
@@ -1,12 +1,12 @@
 (function() {
-	var enableTiles = function(tiles){
+	/*var enableTiles = function(tiles){
 		$('.tile').addClass('disabled');
 		var empty_start = game.locatesEmptySpace(tiles);
 		var to_enable = game.getsLegalMoves(empty_start);
 		for (i=0; i<to_enable.length; i++){
 			$('.placeholder_'+ to_enable[i]+ ' .tile').removeClass('disabled');
 		}
-	};
+	};*/
 	$('.tile_container').on('click', '.tile:not(.disabled)', function(e){
 		var blank_html = '<div class="tile blank_tile disabled"><p>0</p></div>';
 		var empty_tile = $('.blank_tile');
@@ -15,8 +15,14 @@
 		var swap_html = '<div class="tile disabled tile_'+num+'"><p>'+ num +'</p></div>'
 		$('.tile_'+num).replaceWith(blank_html);
 		$(empty_tile).replaceWith(swap_html);
-		var board_tiles = $('#puzzle .tile p').text();
-		var tiles = game.readBoard(board_tiles);
+		var board_tiles = $('#puzzle .tile p');
+		var x = []
+		for (i=0;i<board_tiles.length; i++){
+			x.push(board_tiles[i].textContent);
+		}
+		var tiles = game.readBoard(x);
+		console.log('tiles');
+		console.log(tiles);
 		game.isWinner(tiles);
 		enableTiles(tiles);
 	});
